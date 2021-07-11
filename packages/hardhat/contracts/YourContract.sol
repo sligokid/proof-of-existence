7610b3a7bbeefd1bd3dae2dac0d1c1b3c84056ad
Kieran Mc Gowan <eirwigproject@gmail.com>
2021-07-11 02:58:54 +0100

    adding eventing

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,35 +5,39 @@ import "hardhat/console.sol";
 
 contract YourContract {
 
-  mapping (bytes32 => uint256) private proofs;
+    mapping(bytes32 => uint256) private proofs;
 
-  // FIXME emit a message instead
-  bool public exists = false;
-  uint256 public when;
+    // FIXME emit a message instead
+    bool public exists = false;
+    uint256 public when;
 
-  function check(string memory document) public returns (bool) {
-    bytes32 proof = getHash(document);
-    // Do not complete if not found
-    require(exists = search(proof), "NOT FOUND");
-    //emit ("Proof was resgistered at:");
-    when = proofs[proof];
-    return exists;
-  }
+    event SetPurpose(address sender, address friend, bool isFriend);
 
-  function record(string memory document) public {
-    // Do not register a if already registered
-    bytes32 proof = getHash(document);
-    require(proofs[proof] == 0, "ALREADY REGISTERD");
-    proofs[proof] = block.timestamp;
-    //emit ("Proof exists as of now: ");
-  }
+    function check(string memory document) public returns (bool) {
+        bytes32 proof = getHash(document);
+        // Do not complete if not found
+        require(exists = search(proof), "NOT FOUND");
+        when = proofs[proof];
+        //emit ("Proof was resgistered at:");
+        emit SetPurpose(msg.sender, msg.sender, true);
+        return exists;
+    }
 
-  function getHash(string memory document) private returns (bytes32) {
-    return sha256(abi.encode(document));
-  }
+    function record(string memory document) public {
+        emit SetPurpose(msg.sender, msg.sender, true);
+        // Do not register a if already registered
+        bytes32 proof = getHash(document);
+        require(proofs[proof] == 0, "ALREADY REGISTERD");
+        proofs[proof] = block.timestamp;
+        //emit ("Proof exists as of now: ");
+    }
 
-  function search(bytes32 proof) private returns(bool) {
-    return proofs[proof] != 0;
-  }
+    function getHash(string memory document) private returns (bytes32) {
+        return sha256(abi.encode(document));
+    }
+
+    function search(bytes32 proof) private returns (bool) {
+        return proofs[proof] != 0;
+    }
 
 }
--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,12 +1,12 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import WalletLink from "walletlink";
-import { Alert, Button, Col, Menu, Row } from "antd";
+import {Alert, Button, Col, List, Menu, Row} from "antd";
 import "antd/dist/antd.css";
 import React, { useCallback, useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Web3Modal from "web3modal";
 import "./App.css";
-import { Account, Contract, Faucet, GasGauge, Header, Ramp, ThemeSwitch } from "./components";
+import {Account, Address, Contract, Faucet, GasGauge, Header, Ramp, ThemeSwitch} from "./components";
 import { INFURA_ID, NETWORK, NETWORKS } from "./constants";
 import { Transactor } from "./helpers";
 import {
@@ -420,6 +420,22 @@ function App(props) {
             </Link>
           </Menu.Item>
         </Menu>
+
+        <div style={{ width: 600, margin: "auto", marginTop: 32, paddingBottom: 32 }}>
+          <h2>Events:</h2>
+          <List
+            bordered
+            dataSource={setPurposeEvents}
+            renderItem={item => {
+              return (
+                <List.Item key={item.blockNumber + "_" + item.sender + "_" + item.purpose}>
+                  <Address address={item[0]} ensProvider={mainnetProvider} fontSize={16} />
+                  {item[1]}
+                </List.Item>
+              );
+            }}
+          />
+        </div>
 
         <Switch>
           <Route exact path="/">
