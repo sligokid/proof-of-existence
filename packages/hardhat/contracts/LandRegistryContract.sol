10354ddcbb587e5ce9c1a0de1455766a78a688d3
Kieran Mc Gowan <eirwigproject@gmail.com>
2021-07-11 13:25:39 +0100

    add support for changing ownership

--- packages/hardhat/contracts/LandRegistryContract.sol
+++ packages/hardhat/contracts/LandRegistryContract.sol
@@ -1,19 +1,17 @@
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LandRegistry {
+contract LandRegistry is Ownable {
 
     mapping(bytes32 => uint256) private proofs;
 
     event UpdateEventWindow(address sender, string message);
 
-    address public owner;
-
     constructor(address _owner) public {
-        owner = _owner;
-        console.log("Smart Contract Wallet is owned by:",owner);
+        transferOwnership(_owner);
+        console.log("Smart Contract Wallet is owned by:",_owner);
     }
 
     function verifyTitle(string memory title) public returns (bool) {
@@ -26,8 +24,8 @@ contract LandRegistry {
         return true;
     }
 
-    function registerTitle(string memory title) public {
-        require(msg.sender==owner,"NOT THE OWNER!");
+    function registerTitle(string memory title) public onlyOwner {
+        //require(msg.sender==owner,"NOT THE OWNER!");
         // FIXME only the Registrar can register a title
         // Do not register a if already registered
         bytes32 proof = getHash(title);
